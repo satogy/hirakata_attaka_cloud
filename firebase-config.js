56583c3e778f65d3a73556696c009f9f7a6adba5
satogy <sat.okuda@gmail.com>
2026-07-08 11:05:36 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDtcHo5fIX-hB2nq4QFhivpk7fV1Bw-Wv4",
+  authDomain: "monomatch-pilot.firebaseapp.com",
+  projectId: "monomatch-pilot",
+  storageBucket: "monomatch-pilot.firebasestorage.app",
+  messagingSenderId: "587742780507",
+  appId: "1:587742780507:web:b60235fcd59fa8773ab37e",
+  measurementId: "G-PVKKZSXLZ2"
+};
+
+export const ACCESS_CODE = "";
